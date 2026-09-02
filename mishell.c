0703e57f11ad8b944582d976eb2f33a0bd35380e
BenjaS <bsaavedra2024@inf.udec.cl>
2026-09-01 21:14:28 -0400

    feat: mostrar directorio actual

--- mishell.c
+++ mishell.c
@@ -3,9 +3,26 @@
 #include <string.h>
 #include <sys/types.h>
 #include "shell.h"
+#include <unistd.h>
+
+
+//  gcc mishell.c -o mishell
+//  ./mishell
+
 
 int main(void) {
     
+    //ruta actual con getcwd
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current working directory: %s\n", cwd);
+    } else {
+        perror("getcwd() error");
+        return 1;
+    }
+
+
+
     while (1) {
         char line[MAX_LINE];
         printf("miShell> ");
@@ -17,5 +34,8 @@ int main(void) {
 
         printf("You entered: %s\n", line);
     }
+
+
+
     return 0;
 }
